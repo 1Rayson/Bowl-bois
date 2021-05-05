9cea0d506a31bc44dfa56c9d99daf470e38ee104
Tobias-GitHub <70742954+Tobias-GitHub@users.noreply.github.com>
2021-05-05 10:56:45 +0200

    Small adjustments on several screens
    - Updated the loading-screen.js
    - Added new JS to nearest-location
    - Made a module section in the styleguide.

--- styles/modules/loading-screen/loadingscreen.js
+++ styles/modules/loading-screen/loadingscreen.js
@@ -1,16 +1,16 @@
-console.log('hello')
+// JS that fetches data from server displays random useless facts.
 
-onload();
+randomFact();
 
-function onload(){
+function randomFact(){
     fetch('https://uselessfacts.jsph.pl/random.json?language=en')
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
-        let output = '';
-            output +=`
-            <p>${data.text}</p>
-            `
-        document.querySelector('.insertFact').innerHTML = output;
+        let output = data.text;
+        document.querySelector('.random-fact__content').textContent = output;
     })
 }; 
+
+// Pop up notifications that asks the user premission to use location data.
+
---
+++ styles/modules/nearest-location/nearest-location.js
@@ -0,0 +1,27 @@
+
+// JS that removes the bottom fade when the user scrolls to the bottom.
+
+/* JWB Example */
+      // Inspired by https://stackoverflow.com/a/34550171/1620719
+
+
+      // get DOM node reference
+      const locations = document.querySelector('.locations');
+      const bottom_fade = document.querySelector('.locations__bottom-fade');
+      // setup scroll event
+      locations.addEventListener('scroll', function(event)
+        {
+            let locations = event.target;
+            if (locations.scrollHeight - locations.scrollTop === locations.clientHeight)
+            {
+                console.log('scrolled to bottom');
+                // add class to bottom_fade element when scrolled to bottom
+                bottom_fade.classList.add('--hide');
+            } else {
+                // else ensure class is not present
+                bottom_fade.classList.remove('--hide');
+            }
+        } 
+      );
+
+
